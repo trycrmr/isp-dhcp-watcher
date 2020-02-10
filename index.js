8aa5a7c956060fed0a4fd9607da3f6c0d4bf362a
trycrmr <trycrmr@gmail.com>
2020-02-10 13:31:45 -0500

    Init structure for handling terminal command arguments

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 import usingNode13 from './app-utils/node13Check/index.js'
 
-(async (args) => {
+const ISPDHCPWatcher = async () => {
   try {
     if(!(await usingNode13())) {
       console.log('This script was written to use Node 13. Please use Node 13 or submit a PR for another version of Node.')
       process.exit
     } else {
-      
+      console.log('running ISPDHCPWatcher')
     }
   } catch(err) {
     console.error(err)
     process.exit
   }
+}
 
-  // usingNode13()
-  // .then(result => console.log(`Success: ${result}`), result => console.log(`Exception occurred: ${result}`))
-  // .catch(err => console.log(`Error occurred: ${err}`))
-  // .finally(() => { process.exit })
+export default ISPDHCPWatcher
 
-})(...process.argv)
+switch(process.argv[2]) {
+  case 'test': 
+    console.log('running tests')
+    // import runTests from './test/index.js'
+    // runTests()
+    break
+  default: 
+    ISPDHCPWatcher()
+}
