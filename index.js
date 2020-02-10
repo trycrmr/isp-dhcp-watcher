0314195f40e4ca7f110c6240049016b6ecf69ee0
trycrmr <trycrmr@gmail.com>
2020-02-10 17:20:40 -0500

    Adds notes for future dev

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ const ISPDHCPWatcher = async () => {
         reject('This script was written to use Node 13. Please use Node 13 or submit a PR for another version of Node.')
       } else {
         resolve('running ISPDHCPWatcher')
+
+        /* BIG DREAMS
+        get my current IP
+        compare it to the last IP result 
+        if it didn't change, ping again after a certain interval
+        if it did change: 
+          pause pinging for IP changes (?necessary?)
+          update aws route 53 DNS record with the new IP
+            if that fails, email me?
+            if that is successful, resume the intervals, and email me?
+        
+        if the IP hasn't changed for 8640 tries (presuming a ping interval every 10 seconds), email me that it hasn't, break the loop, and recursively call the function to start checking the IP again. This will provide me a daily update that the script is still working and keep the variable counting the loops from getting really long and eventually breaking. 
+
+        Maybe put a timeout on the network requesting loop if the network requests are timing out. That probably means my internet is down and I wouldn't want some endpoint to get flooded with requests from me when my internet came back up. Then it could check each minute if the internet is back up. When the internet is back up, resume pinging. 
+
+        Can this run on a Raspberry Pi? Specifically, my Raspberry Pi? 
+
+        Start this script on startup of whatever it runs on. 
+
+        Hide the appropriate configs. Use an AWS key with limited privledges (obvi) and email from something@terrycreamer.codes using AWS SES .
+        
+        */
       }
     } catch(err) {
       reject(err)
