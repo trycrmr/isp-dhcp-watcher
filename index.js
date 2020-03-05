0c4dac9486cda5733a499cd01b335e441933f098
trycrmr <trycrmr@gmail.com>
2020-03-05 14:33:06 -0500

    Updates error output and adds config imports

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import usingNode13 from './appUtils/node13Check/index.js'
 import checkPublicIP from './appUtils/checkPublicIP/index.js'
-
+import { customNetworkTimeoutThreshold, customUrlForIPCheck, DNSAPIDetails } from './configs/index.js'
 import runTests from './test/index.js'
 import tests from './test/tests.js'
 
-const NETWORK_TIMEOUT_THRESHOLD_MS = 5000
-const URL_FOR_IP_CHECK = 'http://ipecho.net/plain'
-const DELAY_BETWEEN_IP_CHECKS = 10000
+console.log(customUrlForIPCheck, customNetworkTimeoutThreshold)
+const NETWORK_TIMEOUT_THRESHOLD_MS = customNetworkTimeoutThreshold ? customNetworkTimeoutThreshold : 20000
+const URL_FOR_IP_CHECK = customUrlForIPCheck ? customUrlForIPCheck : 'http://ipecho.net/plain'
+const DELAY_BETWEEN_IP_CHECKS = 2000
 const delay = (delayMS = DELAY_BETWEEN_IP_CHECKS) => {
   return new Promise((resolve, reject) => {
     console.log(`===> Delaying for ${DELAY_BETWEEN_IP_CHECKS / 1000} seconds, then reattempting network call.`)
@@ -48,7 +49,7 @@ const ISPDHCPWatcher = async () => {
     while(!issue) {
       try {
         const checkPublicIPResult = await checkPublicIP(URL_FOR_IP_CHECK, { timeout: NETWORK_TIMEOUT_THRESHOLD_MS })
-        .catch(err => new CheckIPError(err))
+        .catch(err =>{ console.error(err); return new CheckIPError(err) })
 
         if(checkPublicIPResult instanceof Error) throw checkPublicIPResult
 
@@ -74,7 +75,8 @@ const ISPDHCPWatcher = async () => {
         }
       } catch(err) {
           if(err instanceof CheckIPError) {
-            console.log(`===> Error issuing network call. Delaying for ${DELAY_BETWEEN_IP_CHECKS / 1000} seconds, then reattempting network call.`)
+            console.error(`===> Error issuing network call. Delaying for ${DELAY_BETWEEN_IP_CHECKS / 1000} seconds, then reattempting network call.
+`, err)
             await delay()
             continue
           } else {
